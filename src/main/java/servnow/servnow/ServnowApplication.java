050560e90b65ebf45742ee19573ac458be09a21e
HyunwooYi <83715335+HyunwooYi@users.noreply.github.com>
2024-07-19 02:17:01 +0900

    test setting commit

---
+++ src/main/java/servnow/servnow/ServnowApplication.java
@@ -0,0 +1,13 @@
+package servnow.servnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServnowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServnowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/servnow/servnow/ServnowApplicationTests.java
@@ -0,0 +1,13 @@
+package servnow.servnow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServnowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

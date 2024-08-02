2b6d079e4c7c3a98a1e9de9d9ba855d2a9ba01f5
PicturePark1101 <seojin5565@gmail.com>
2024-08-02 23:23:43 +0900

    [refactor] 불필요한 주석 제거 #12

--- src/main/java/servnow/servnow/api/survey/service/SurveyCommandService.java
+++ src/main/java/servnow/servnow/api/survey/service/SurveyCommandService.java
@@ -1,6 +1,5 @@
 package servnow.servnow.api.survey.service;
 
-import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,12 +7,12 @@ import org.springframework.transaction.annotation.Transactional;
 import servnow.servnow.api.question.service.MultipleChoiceUpdater;
 import servnow.servnow.api.question.service.QuestionUpdater;
 import servnow.servnow.api.section.service.SectionUpdater;
-import servnow.servnow.api.survey.dto.SurveyPostRequest;
-import servnow.servnow.api.survey.dto.SurveyPostRequest.SurveyPostSection;
-import servnow.servnow.api.survey.dto.SurveyPostRequest.SurveyPostSection.SurveyPostQuestion;
-import servnow.servnow.api.survey.dto.SurveyPostRequest.SurveyPostSection.SurveyPostQuestion.SurveyPostAnswer;
+import servnow.servnow.api.survey.dto.request.SurveyPostRequest;
+import servnow.servnow.api.survey.dto.request.SurveyPostRequest.SurveyPostSection;
+import servnow.servnow.api.survey.dto.request.SurveyPostRequest.SurveyPostSection.SurveyPostQuestion;
+import servnow.servnow.api.survey.dto.request.SurveyPostRequest.SurveyPostSection.SurveyPostQuestion.SurveyPostAnswer;
 import servnow.servnow.api.user.service.UserFinder;
-import servnow.servnow.domain.question.model.MultipleChoice;
+import servnow.servnow.api.user.service.UserInfoFinder;
 import servnow.servnow.domain.question.model.Question;
 import servnow.servnow.domain.question.model.enums.QuestionType;
 import servnow.servnow.domain.section.model.Section;
@@ -23,44 +22,45 @@ import servnow.servnow.domain.survey.model.Survey;
 @RequiredArgsConstructor
 public class SurveyCommandService {
 
+  private final UserFinder userFinder;
+  private final UserInfoFinder userInfoFinder;
   private final SurveyUpdater surveyUpdater;
   private final SectionUpdater sectionUpdater;
-  private final UserFinder userFinder;
   private final MultipleChoiceUpdater multipleChoiceUpdater;
   private final QuestionUpdater questionUpdater;
 
   @Transactional
   public void createSurvey(final long userId, final SurveyPostRequest surveyPostRequest) {
-
     Survey survey = surveyUpdater.save(surveyPostRequest.toEntity(userFinder.findById(userId)));
-
-    List<Question> mapToQuestions = new ArrayList<>();
-    List<MultipleChoice> mapToMultipleChoices = new ArrayList<>();
-;
-    sectionUpdater.saveAll(convertToSections(survey, surveyPostRequest.sections(), mapToQuestions, mapToMultipleChoices));
-    questionUpdater.saveAll(mapToQuestions);
-    multipleChoiceUpdater.saveAll(mapToMultipleChoices);
+    saveSectionsAndQuestions(survey, surveyPostRequest.sections());
+    updateUserPoint(userId);
   }
 
-  private List<Section> convertToSections(final Survey survey, final List<SurveyPostSection> surveyPostSection, final List<Question> mapToQuestions, final List<MultipleChoice> mapToMultipleChoices) {
-    return surveyPostSection.stream().map(s ->{
+  private void updateUserPoint(final long userId) {
+    userInfoFinder.findByUserId(userId).updatePoint(200);
+  }
+
+  public void saveSectionsAndQuestions(final Survey survey, final List<SurveyPostSection> surveyPostSections) {
+    surveyPostSections.forEach(s -> {
       Section section = s.toEntity(survey);
-      mapToQuestions.addAll(convertToQuestions(section, s.questions(), mapToMultipleChoices));
-      return section;
-    }).toList();
+      sectionUpdater.save(section);
+      saveQuestions(section, s.questions());
+    });
   }
 
-  private List<Question> convertToQuestions(final Section section, final List<SurveyPostQuestion> surveyPostQuestions, final List<MultipleChoice> mapToMultipleChoices) {
-    return surveyPostQuestions.stream().map(q -> {
+  public void saveQuestions(final Section section, final List<SurveyPostQuestion> surveyPostQuestions) {
+    surveyPostQuestions.forEach(q -> {
       Question question = q.toEntity(section);
+      questionUpdater.save(question);
       if (q.questionType().equals(QuestionType.MULTIPLE_CHOICE)) {
-        mapToMultipleChoices.addAll(convertToMultipleChoices(question, q.answers()));
+        saveMultipleChoices(question, q.answers());
       }
-      return question;
-    }).toList();
+    });
   }
 
-  private List<MultipleChoice> convertToMultipleChoices(final Question question, final List<SurveyPostAnswer> surveyPostAnswers) {
-      return surveyPostAnswers.stream().map(it -> it.toEntity(question)).toList();
+  public void saveMultipleChoices(final Question question, final List<SurveyPostAnswer> surveyPostAnswers) {
+    surveyPostAnswers.stream()
+        .map(answerDto -> answerDto.toEntity(question))
+        .forEach(multipleChoiceUpdater::save);
   }
 }

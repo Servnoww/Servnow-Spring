6ac497d8753aa8399e005b4dc03d58cde1a95e98
PicturePark1101 <seojin5565@gmail.com>
2024-08-02 22:18:21 +0900

    [feat] service 구현 #12

--- src/main/java/servnow/servnow/api/survey/controller/SurveyController.java
+++ src/main/java/servnow/servnow/api/survey/controller/SurveyController.java
@@ -1,15 +1,30 @@
 package servnow.servnow.api.survey.controller;
 
+import jakarta.validation.Valid;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import servnow.servnow.api.dto.ServnowResponse;
+import servnow.servnow.api.survey.dto.SurveyPostRequest;
 import servnow.servnow.api.survey.service.SurveyCommandService;
 import servnow.servnow.api.survey.service.SurveyQueryService;
+import servnow.servnow.common.code.CommonSuccessCode;
 
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class SurveyController {
+
   private final SurveyCommandService surveyCommandService;
-  private final SurveyQueryService surveyQueryService;
+
+  @PostMapping("/survey")
+  public ServnowResponse<Void> createSurvey(@RequestBody @Valid SurveyPostRequest surveyPostRequest) {
+    // 유저 임시생성, 추후 아이디 로직 머지 후 고칠 예정
+    System.out.println("왜안됨" +LocalDateTime.now());
+    surveyCommandService.createSurvey(1L, surveyPostRequest);
+    return ServnowResponse.success(CommonSuccessCode.CREATED);
+  }
 }

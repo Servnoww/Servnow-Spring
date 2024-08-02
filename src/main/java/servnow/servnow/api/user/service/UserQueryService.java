757a79e85256c049fd9510b021e08e020a2aaae7
HyunwooYi <83715335+HyunwooYi@users.noreply.github.com>
2024-08-03 02:09:38 +0900

    [fea] UserQueryService 구현중

--- src/main/java/servnow/servnow/api/user/service/UserQueryService.java
+++ src/main/java/servnow/servnow/api/user/service/UserQueryService.java
@@ -2,8 +2,49 @@ package servnow.servnow.api.user.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import servnow.servnow.api.user.service.response.EditProfilePageResponse;
+import servnow.servnow.api.user.service.response.KakaoEditProfilePageResponse;
+import servnow.servnow.api.user.service.response.MyPageResponse;
+import servnow.servnow.api.user.service.response.ServnowEditProfilePageResponse;
+import servnow.servnow.common.code.UserMyPageErrorCode;
+import servnow.servnow.common.exception.NotFoundException;
+import servnow.servnow.domain.user.model.User;
+import servnow.servnow.domain.user.model.UserInfo;
+import servnow.servnow.domain.user.model.enums.Platform;
+import servnow.servnow.domain.user.repository.UserInfoRepository;
+import servnow.servnow.domain.user.repository.UserRepository;
 
 @Service
 @RequiredArgsConstructor
 public class UserQueryService {
+
+    private final UserInfoRepository userInfoRepository;
+    private final UserRepository userRepository;
+
+    public MyPageResponse getMyPage() {
+        User user = userRepository.findById(3L)
+                .orElseThrow(() -> new NotFoundException(UserMyPageErrorCode.USER_NOT_FOUND));
+        UserInfo userinfo = userInfoRepository.findById(1L)
+                .orElseThrow(() -> new NotFoundException(UserMyPageErrorCode.USER_NOT_FOUND));
+        return MyPageResponse.of(userinfo, user);
+    }
+
+    public EditProfilePageResponse getEditProfilePage() {
+        User user = userRepository.findById(4L)
+                .orElseThrow(() -> new NotFoundException(UserMyPageErrorCode.USER_NOT_FOUND));
+        UserInfo userInfo = userInfoRepository.findById(4L)
+                .orElseThrow(() -> new NotFoundException(UserMyPageErrorCode.USER_NOT_FOUND));
+
+        if (user.getPlatform() == Platform.KAKAO) {
+            return KakaoEditProfilePageResponse.of(userInfo);
+        } else if (user.getPlatform() == Platform.SERVNOW) {
+            return ServnowEditProfilePageResponse.of(userInfo, user);
+        } else {
+            throw new NotFoundException(UserMyPageErrorCode.PLATFORM_NOT_FOUND);
+        }
+    }
+
+    public boolean emailDuplicate(String email) {
+        return userInfoRepository.existsByEmail(email);
+    }
 }

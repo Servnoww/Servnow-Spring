f5236dd47f3fe30c87b963e1003c80db69ad5177
PicturePark1101 <seojin5565@gmail.com>
2024-08-04 03:07:44 +0900

    [feat] responseDTO 셋팅 #13

---
+++ src/main/java/servnow/servnow/api/survey/dto/response/SurveyGetResponse.java
@@ -0,0 +1,74 @@
+package servnow.servnow.api.survey.dto.response;
+
+import java.util.List;
+import servnow.servnow.domain.question.model.MultipleChoice;
+import servnow.servnow.domain.question.model.Question;
+import servnow.servnow.domain.question.model.enums.QuestionType;
+import servnow.servnow.domain.section.model.Section;
+
+public record SurveyGetResponse(
+    long surveyId,
+    String sectionTitle,
+    String sectionContent,
+    int sectionOrder,
+    int sectionTotalCount,
+    int nextSectionNo,
+    List<SurveyGetQuestion> questions
+) {
+
+  public static SurveyGetResponse of(final Section section, final int sectionTotalCount) {
+    return new SurveyGetResponse(
+        section.getSurvey().getId(),
+        section.getTitle(),
+        section.getContent(),
+        section.getSectionOrder(),
+        sectionTotalCount,
+        section.getNextSectionNo(),
+        section.getQuestions().stream().map(SurveyGetQuestion::of).toList()
+        );
+  }
+
+  public record SurveyGetQuestion(
+      long questionId,
+      String questionTitle,
+      String questionContent,
+      int questionOrder,
+      QuestionType questionType,
+      boolean isEssential,
+      boolean isDuplicate,
+      boolean hasNextSection,
+      List<SurveyGetAnswer> answers
+  ){
+    public static SurveyGetQuestion of(final Question question) {
+      return new SurveyGetQuestion(
+          question.getId(),
+          question.getTitle(),
+          question.getContent(),
+          question.getQuestionOrder(),
+          question.getQuestionType(),
+          question.isEssential(),
+          question.isDuplicate(),
+          question.isHasNextSection(),
+          getMultipleChoicesOrNull(question));
+    }
+
+    private static List<SurveyGetAnswer> getMultipleChoicesOrNull(final Question question){
+        if (question.getQuestionType().equals(QuestionType.MULTIPLE_CHOICE)){
+          return question.getMultipleChoices().stream().map(SurveyGetAnswer::of).toList();
+        }
+        return null;
+    }
+
+    public record SurveyGetAnswer(
+      String answerContent,
+      Integer nextSectionNo
+    ) {
+      public static SurveyGetAnswer of(final MultipleChoice multipleChoice) {
+        return new SurveyGetAnswer(
+            multipleChoice.getContent(),
+            multipleChoice.getNextSectionNo()
+        );
+      }
+    }
+  }
+}

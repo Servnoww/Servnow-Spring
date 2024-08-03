c161386c13e9fecd17a6cf6da7bbfeef6797f7d6
PicturePark1101 <seojin5565@gmail.com>
2024-08-03 16:39:32 +0900

    [feat] controller 셋팅 #14

--- src/main/java/servnow/servnow/api/survey/controller/SurveyController.java
+++ src/main/java/servnow/servnow/api/survey/controller/SurveyController.java
@@ -2,14 +2,20 @@ package servnow.servnow.api.survey.controller;
 
 import jakarta.validation.Valid;
 import java.time.LocalDateTime;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import servnow.servnow.api.dto.ServnowResponse;
 import servnow.servnow.api.survey.dto.request.SurveyPostRequest;
+import servnow.servnow.api.survey.dto.response.SurveyIntroGetResponse;
 import servnow.servnow.api.survey.service.SurveyCommandService;
+import servnow.servnow.api.survey.service.SurveyQueryService;
 import servnow.servnow.common.code.CommonSuccessCode;
 
 @RestController
@@ -18,11 +24,18 @@ import servnow.servnow.common.code.CommonSuccessCode;
 public class SurveyController {
 
   private final SurveyCommandService surveyCommandService;
+  private final SurveyQueryService surveyQueryService;
+
 
   @PostMapping("/survey")
   public ServnowResponse<Void> createSurvey(@RequestBody @Valid SurveyPostRequest surveyPostRequest) {
     // 유저 임시생성, 추후 아이디 로직 머지 후 고칠 예정
     surveyCommandService.createSurvey(1L, surveyPostRequest);
     return ServnowResponse.success(CommonSuccessCode.CREATED);
+  }
+
+  @GetMapping("/survey/{id}/intro")
+  public ServnowResponse<SurveyIntroGetResponse> getSurveyIntro(@PathVariable(name = "id") long id) {
+    return ServnowResponse.success(CommonSuccessCode.OK, surveyQueryService.getSurveyIntro(null, id));
   }
 }

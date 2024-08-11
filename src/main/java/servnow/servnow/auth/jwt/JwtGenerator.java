9d3ebb7e7c68f1f36d7b8edb5537ff931fa7743b
dkyuuum <dkyuuum@gmail.com>
2024-08-12 08:33:26 +0900

    [fix] jwt parsing 오류 해결

--- src/main/java/servnow/servnow/auth/jwt/JwtGenerator.java
+++ src/main/java/servnow/servnow/auth/jwt/JwtGenerator.java
@@ -1,6 +1,7 @@
 package servnow.servnow.auth.jwt;
 
 import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Encoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -8,7 +9,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.security.Key;
+import java.util.Arrays;
 import java.util.Base64;
 import java.util.Date;
 
@@ -17,46 +20,36 @@ import java.util.Date;
 public class JwtGenerator {
     @Value("${spring.jwt.secret.key}")
     private String JWT_SECRET;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60; 	//1시간
     private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 14;  // 14일
 
     public static final String USER_ROLE_CLAIM_NAME = "role";
 
-    public JwtParser getJwtParser() {
-        return Jwts.parserBuilder()
-                .setSigningKey(getSigningKey())
-                .build();
-    }
-
-    private Key getSigningKey() {
-//        byte[] keyBytes = Base64.getUrlDecoder().decode(JWT_SECRET);
-//        return Keys.hmacShaKeyFor(keyBytes);
-        return Keys.secretKeyFor(SignatureAlgorithm.HS512);
-    }
-
-    private long calculateExpirationTime(boolean isAccessToken) {
-        if (isAccessToken) {
-            return ACCESS_TOKEN_EXPIRE_TIME;
-        }
-        return REFRESH_TOKEN_EXPIRE_TIME;
-    }
-    public String generateToken(Long serialId, String role, boolean isAccessToken) {
+    public String generateToken(Long userId, String role, boolean isAccessToken) {
         final Date now = generateNowDate();
         final Date expiration = generateExpirationDate(isAccessToken, now);
 
-        Claims claims = Jwts.claims().setSubject(String.valueOf(serialId));
+        Claims claims = Jwts.claims().setSubject(String.valueOf(userId));
         if (isAccessToken) {
             claims.put(USER_ROLE_CLAIM_NAME, role);
         }
-
+        logger.info("okay");
         return Jwts.builder()
                 .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
                 .setClaims(claims)
                 .setIssuedAt(now)
                 .setExpiration(expiration)
-                .signWith(getSigningKey(), SignatureAlgorithm.HS512)
+                .signWith(SignatureAlgorithm.HS256, getSigningKey())
                 .compact();
+    }
+
+    public JwtParser getJwtParser() {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+//                .setSigningKey(JWT_SECRET.getBytes())
+                .build();
     }
 
     private Date generateNowDate() {
@@ -65,5 +58,22 @@ public class JwtGenerator {
 
     private Date generateExpirationDate(boolean isAccessToken, Date now) {
         return new Date(now.getTime() + calculateExpirationTime(isAccessToken));
+    }
+
+    private Key getSigningKey() {
+         try {
+            byte[] keyBytes = Base64.getDecoder().decode(JWT_SECRET);
+            return Keys.hmacShaKeyFor(keyBytes);
+        } catch (IllegalArgumentException e) {
+            logger.error("JWT_SECRET 값이 유효한 Base64 인코딩이 아닙니다: {}", e.getMessage());
+            throw e;
+        }
+    }
+
+    private long calculateExpirationTime(boolean isAccessToken) {
+        if (isAccessToken) {
+            return ACCESS_TOKEN_EXPIRE_TIME;
+        }
+        return REFRESH_TOKEN_EXPIRE_TIME;
     }
 }

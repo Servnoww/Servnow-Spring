19b6d3810dd54753cbaae0bfe77ab46c7cd1cf4b
HyunwooYi <83715335+HyunwooYi@users.noreply.github.com>
2024-08-23 04:47:11 +0900

    [fix] 카카오 로그인시 마이페이지 수정 반환값

--- src/main/java/servnow/servnow/api/user/dto/response/KakaoEditProfilePageResponse.java
+++ src/main/java/servnow/servnow/api/user/dto/response/KakaoEditProfilePageResponse.java
@@ -1,15 +1,15 @@
 package servnow.servnow.api.user.dto.response;
 
 import servnow.servnow.domain.user.model.UserInfo;
+import servnow.servnow.domain.user.model.enums.Platform;
 
 public record KakaoEditProfilePageResponse(
         String profile_url,
-        String nickname
-//        String name,
-//        String phone
+        String nickname,
+        Platform platform
 ) implements EditProfilePageResponse{
     public static KakaoEditProfilePageResponse of (UserInfo userInfo) {
-        return new KakaoEditProfilePageResponse(userInfo.getNickname(), userInfo.getProfile_url());
+        return new KakaoEditProfilePageResponse(userInfo.getProfile_url(), userInfo.getNickname(), userInfo.getUser().getPlatform());
                 // 카카오는 이름, 번호가 필요한데 해당 창이 없음
     }
 }

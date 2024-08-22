441b28398e26726563b5dfacb82485db9ac42419
PicturePark1101 <seojin5565@gmail.com>
2024-08-22 15:57:41 +0900

    [fix] 어노테이션 추가해서 시도 #98

--- src/main/java/servnow/servnow/api/user/controller/UserController.java
+++ src/main/java/servnow/servnow/api/user/controller/UserController.java
@@ -1,12 +1,14 @@
 package servnow.servnow.api.user.controller;
 
 import io.swagger.v3.oas.annotations.Parameter;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
-import lombok.Value;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -130,7 +132,7 @@ public class UserController {
         return ServnowResponse.success(CommonSuccessCode.OK);
     }
 
-    @PatchMapping("/users/me/survey/memo")
+    @PatchMapping ("/users/me/survey/memo")
     public ServnowResponse<Void> updateSurveyMemos(@RequestBody SurveyResultMemosPatchRequest surveyResultMemosPatchRequest) {
         resultCommandService.updateSurveyMemos(surveyResultMemosPatchRequest);
         return ServnowResponse.success(CommonSuccessCode.OK);
